360767698ff2d176ee299edc743ed20272033cdc
arcticmonkey07 <arcticmonkey07@gmail.com>
2020-08-01 00:26:54 +0300

    Updates

---
+++ precache-manifest.3f058108b70bd4c215d1258b7ca58aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50aff37019dfed77195169762fa74b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d08b8e41beb6786e156",
+    "url": "./static/css/main.e216dbce.chunk.css"
+  },
+  {
+    "revision": "cfb1fe8035968c0b6662",
+    "url": "./static/js/2.8b6ba72f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "./static/js/2.8b6ba72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d08b8e41beb6786e156",
+    "url": "./static/js/main.075e785d.chunk.js"
+  },
+  {
+    "revision": "8e3be9b8081ccebaffef",
+    "url": "./static/js/runtime-main.93448219.js"
+  }
+]);
